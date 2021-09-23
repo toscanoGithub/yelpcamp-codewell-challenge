6c35a500782fe8ce8598505edb2e721d0f087b34
AIT ASSOU ABDELKADER <pasarela70@hotmail.com>
2021-09-23 14:23:27 -0400

    fixing cors

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: "include", // include, *same-origin, omit
+        credentials: "*same-origin", // include, *same-origin, omit
         headers: {
           "Content-Type": "application/json",
           "access-control-allow-origin": "*",
