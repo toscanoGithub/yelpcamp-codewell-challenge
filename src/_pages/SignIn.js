ec85cf757e35f83393bcc95ac45d409ab11c72f0
AIT ASSOU ABDELKADER <pasarela70@hotmail.com>
2021-09-23 22:36:58 -0400

    fixing cors

--- src/_pages/SignIn.js
+++ src/_pages/SignIn.js
@@ -202,11 +202,7 @@ function SignIn() {
                   method: "post",
                   url: `${process.env.REACT_APP_API_URL}api/users/login`,
                   withCredentials: true,
-                  headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods":
-                      "GET, PUT, POST, DELETE, PATCH, OPTIONS",
-                  },
+                  
                   data: {
                     ...values,
                   },
