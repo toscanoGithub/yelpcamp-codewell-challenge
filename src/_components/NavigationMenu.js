ba80c3f319295a78fa87eb119fc32bcde99ef42c
AIT ASSOU ABDELKADER <pasarela70@hotmail.com>
2021-09-24 15:57:17 -0400

    fixing cors

--- src/_components/NavigationMenu.js
+++ src/_components/NavigationMenu.js
@@ -67,32 +67,29 @@ const NavigationMenu = () => {
       });
   };
 
-  // useEffect(() => {
-  //   (async function () {
-  //     if (!auth) return;
-  //     fetch(
-  //       `https://yelpcamp-codewell-challenge.herokuapp.com/api/users/${auth}`,
-  //       {
-  //         method: "GET", // *GET, POST, PUT, DELETE, etc.
-  //         mode: "cors", // no-cors, *cors, same-origin
-  //         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-  //         credentials: "include", // include, *same-origin, omit
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           "access-control-allow-origin": "*",
-  //           // 'Content-Type': 'application/x-www-form-urlencoded',
-  //         },
-  //       }
-  //     )
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         setCurrentUser(data);
-  //       })
-  //       .catch((error) => {
-  //         console.log(error.message);
-  //       });
-  //   })();
-  // }, [auth]);
+  useEffect(() => {
+    (async function () {
+      if (!auth) return;
+      fetch(`${process.env.REACT_APP_API_URL}${auth}`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "include", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          "access-control-allow-origin": "*",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setCurrentUser(data);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    })();
+  }, [auth]);
   return (
     <Menu className={classes.menu} right width={250}>
       {currentUser && (
